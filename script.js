715c7650df9e2e0ec037bd66e3aeb8c4b5458e29
Constantinedr <kostalampadaris@gmail.com>
2025-06-02 18:43:49 +0300

    dffd
    f

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButtons = document.querySelectorAll('.toggle-button');
-
   toggleButtons.forEach(button => {
     button.addEventListener('click', () => {
       const sectionContent = button.closest('section').querySelector('.section-content');
@@ -9,11 +8,27 @@ document.addEventListener('DOMContentLoaded', () => {
       sectionContent.classList.toggle('collapsed');
       button.classList.toggle('collapsed');
       
-      // Adjust max-height dynamically for smooth animation
       if (sectionContent.classList.contains('collapsed')) {
         sectionContent.style.maxHeight = '0';
       } else {
         sectionContent.style.maxHeight = sectionContent.scrollHeight + 'px';
+      }
+    });
+  });
+  const projectToggleButtons = document.querySelectorAll('.project-toggle-button');
+  projectToggleButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault(); 
+      const projectContent = button.closest('.project').querySelector('.project-content');
+      const icon = button.querySelector('i');
+      
+      projectContent.classList.toggle('collapsed');
+      button.classList.toggle('collapsed');
+      
+      if (projectContent.classList.contains('collapsed')) {
+        projectContent.style.maxHeight = '0';
+      } else {
+        projectContent.style.maxHeight = projectContent.scrollHeight + 'px';
       }
     });
   });
