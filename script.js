e7c1270a257b8081afb853082e5187d5ea3d2a0b
Constantinedr <kostalampadaris@gmail.com>
2025-06-03 19:55:41 +0300

    fd
    ffddf

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const projectContents = document.querySelectorAll('.project-content');
-  const projectToggleButtons = document.querySelectorAll('.project-toggle-button');
-  
-  projectContents.forEach(content => {
-    content.classList.add('collapsed');
-    content.style.maxHeight = '0';
-  });
-  
-  projectToggleButtons.forEach(button => {
-    button.classList.add('collapsed');
-  });
+  const setupToggleButtons = (buttonSelector, contentSelector, parentSelector) => {
+    const buttons = document.querySelectorAll(buttonSelector);
 
+    buttons.forEach(button => {
+      const parent = button.closest(parentSelector);
+      const content = parent.querySelector(contentSelector);
 
-  const toggleButtons = document.querySelectorAll('.toggle-button');
-  toggleButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const sectionContent = button.closest('section').querySelector('.section-content');
-      sectionContent.classList.toggle('collapsed');
-      button.classList.toggle('collapsed');
-      
-      if (sectionContent.classList.contains('collapsed')) {
-        sectionContent.style.maxHeight = '0';
-      } else {
-        sectionContent.style.maxHeight = sectionContent.scrollHeight + 'px';
-      }
+      button.classList.add('collapsed');
+      content.classList.add('collapsed');
+      content.style.maxHeight = '0';
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
+        const isCollapsed = content.classList.toggle('collapsed');
+        button.classList.toggle('collapsed');
+
+        if (isCollapsed) {
+          content.style.maxHeight = '0';
+        } else {
+          content.style.maxHeight = content.scrollHeight + 'px';
+        }
+      });
     });
-  });
+  };
 
-  projectToggleButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      event.preventDefault();
-      const projectContent = button.closest('.project').querySelector('.project-content');
-      projectContent.classList.toggle('collapsed');
-      button.classList.toggle('collapsed');
-      
-      if (projectContent.classList.contains('collapsed')) {
-        projectContent.style.maxHeight = '0';
-      } else {
-        projectContent.style.maxHeight = projectContent.scrollHeight + 'px';
-      }
-    });
-  });
+  setupToggleButtons('.toggle-button', '.section-content', 'section');
+  setupToggleButtons('.project-toggle-button', '.project-content', '.project');
 });
